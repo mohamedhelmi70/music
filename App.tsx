26367edd2c166acaaff4b90a9c3cd7e34f10a750
Mohamed Helmi <mtabila37@gmail.com>
2021-10-21 21:39:47 +0200

    init app

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+
+function App() {
+  return <SafeAreaView></SafeAreaView>;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
