fdcd7382b69def05685e8cf75e7679419407cd90
Mohamed Helmi <mtabila37@gmail.com>
2021-10-23 04:16:37 +0200

    add  colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,29 @@
+
+const tintColorDark = '#fff';
+
+export default {
+    light: {
+        text: '#121015',
+        gray: '#C1C6C7',
+        background: '#F7F7F9',
+        tint: "#03873B",
+        tabIconDefault: '#C1C6C7',
+        tabIconSelected: '#121015',
+        dark: '#8B899F',
+        primary: '#03873B',
+        success: '#01937C',
+        placeholder: '#aaa',
+    },
+    dark: {
+        text: '#121015',
+        gray: '#C1C6C7',
+        background: '#F7F7F9',
+        tint: "#03873B",
+        tabIconDefault: '#C1C6C7',
+        tabIconSelected: '#121015',
+        dark: '#8B899F',
+        primary: '#03873B',
+        success: '#01937C',
+        placeholder: '#aaa',
+    },
+};
