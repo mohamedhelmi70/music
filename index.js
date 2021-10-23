acf4e70232a0e2eb8ca7cbac510a8f71b86af485
Mohamed Helmi <mtabila37@gmail.com>
2021-10-23 04:14:22 +0200

    add headless

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+function HeadlessCheck({isHeadless}) {
+    if (isHeadless) {
+        // App has been launched in the background by iOS, ignore
+        return null;
+    }
+
+    return <App />;
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
