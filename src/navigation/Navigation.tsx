55d137644f6785c086f5622c8c2c32e191e3297b
Mohamed Helmi <mtabila37@gmail.com>
2021-10-24 21:11:20 +0200

    update screens headder

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -4,6 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {AppParamList} from '../../types';
 import Tabbar from './Tab';
 import {BackButton} from '../components';
+import {wp} from '../constants';
 
 const Stack = createStackNavigator<AppParamList>();
 
@@ -18,8 +19,19 @@ function Navigation() {
                 headerLeft: () => <BackButton color="#000" />,
             }}>
             <Stack.Screen name="Tab" component={Tabbar} options={{headerShown: false}} />
-            <Stack.Screen name="Artist" component={Artist} />
-            <Stack.Screen name="Track" component={Track} />
+            <Stack.Screen name="Artist" component={Artist} options={{headerShown: false}} />
+            <Stack.Screen
+                name="Track"
+                component={Track}
+                options={({route}) => ({
+                    title: route.params.title,
+                    headerTitleAlign: 'center',
+                    headerTitleContainerStyle: {
+                        width: wp('50%'),
+                        alignItems: 'center',
+                    },
+                })}
+            />
         </Stack.Navigator>
     );
 }
